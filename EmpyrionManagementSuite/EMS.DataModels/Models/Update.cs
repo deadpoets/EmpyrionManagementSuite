0ff0ae5d8f0aa96a76d2929da095ea97f9d51c6a
Dylan Legendre <dylanlegendre09@gmail.com>
2016-10-31 23:09:20 -0500

    added update.json

---
+++ EmpyrionManagementSuite/EMS.DataModels/Models/Update.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EMS.DataModels.Models
+{
+    /// <summary>
+    /// Contains all information relevant for updating the application.
+    /// </summary>
+    public class Update
+    {
+        public string LatestVersion { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public string Description { get; set; }
+        public string UpdateType { get; set; }
+        public bool RequiresReinstall { get; set; }
+        public List<string> Changelog { get; set; }
+    }
+}
