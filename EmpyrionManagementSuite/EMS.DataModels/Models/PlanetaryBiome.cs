d704475c19bdd9ab6e165d8b738b6411d06d67bd
Dylan Legendre <dylanlegendre09@gmail.com>
2016-11-04 16:45:27 -0500

    added biome model

---
+++ EmpyrionManagementSuite/EMS.DataModels/Models/PlanetaryBiome.cs
@@ -0,0 +1,10 @@
+﻿namespace EMS.DataModels.Models
+{
+    /// <summary>
+    /// Model representing the EMS implementation of what is stored within a Planetary Biome (playfield) on disk.
+    /// </summary>
+    public class PlanetaryBiome
+    {
+        public string Name { get; set; }
+    }
+}
