d0806e5060867ce7c85b14ba574987d9cb337d9e
Dylan Legendre <dylanlegendre09@gmail.com>
2016-11-13 21:22:34 -0600

    settings page work

--- EmpyrionManagementSuite/EMS.Core/ViewModels/SettingsViewModel.cs
+++ EmpyrionManagementSuite/EMS.Core/ViewModels/SettingsViewModel.cs
@@ -7,10 +7,55 @@ namespace EMS.Core.ViewModels
     {
         private IFrameNavigationService navService;
 
+        private bool shouldCheckForUpdates;
+
+        public string InstallationPathLabel
+        {
+            get
+            {
+                return ((dynamic) Application.Current).GetLocalizationResourceValue("LABEL_GAME_INSTALLATION_PATH");
+            }
+        }
+
+        public string UpdateLabel
+        {
+            get
+            {
+                return ((dynamic) Application.Current).GetLocalizationResourceValue("LABEL_UPDATE_CHECK");
+            }
+        }
+
+        public string InstallationPath
+        {
+            get
+            {
+                return ((dynamic) Application.Current).Settings.GameInstallationPath;
+            }
+
+            set
+            {
+                InstallationPath = value;
+            }
+        }
+
+        public bool ShouldCheckForUpdates
+        {
+            get
+            {
+                return shouldCheckForUpdates;
+            }
+
+            set
+            {
+                shouldCheckForUpdates = value;
+            }
+        }
+
         public SettingsViewModel(IFrameNavigationService NAVSERVICE)
         {
             navService = NAVSERVICE;
             Name = ((dynamic) Application.Current).GetLocalizationResourceValue("APP_NAME");
+            shouldCheckForUpdates = ((dynamic) Application.Current).Settings.CheckForUpdates;
         }
     }
 }
