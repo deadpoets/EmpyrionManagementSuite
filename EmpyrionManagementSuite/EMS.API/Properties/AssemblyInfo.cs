db9b25a8fde5345246cc2a3d8fbf01f8188d5efb
Dylan Legendre <dylanlegendre09@gmail.com>
2016-11-23 14:28:47 -0600

    initial commit of API project

--- EmpyrionManagementSuite/EMS.API/Properties/AssemblyInfo.cs
+++ EmpyrionManagementSuite/EMS.API/Properties/AssemblyInfo.cs
@@ -1,12 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyTitle("EMS.API")]
+[assembly: AssemblyDescription("The web API for interactivity between the EMS cloud databases and a client.")]
 [assembly: AssemblyCompany("MorpheusZero")]
-[assembly: AssemblyProduct("Empyrion Management Suite")]
+[assembly: AssemblyProduct("Empyrion Management Suite API")]
 [assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("EMS")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
+[assembly: Guid("322262db-138b-461d-9da4-a2f822227c45")]
 [assembly: AssemblyVersion("1.0.1")]
 [assembly: AssemblyFileVersion("1.0.1")]
 [assembly: AssemblyInformationalVersion("1.0.1")]
