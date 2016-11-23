3627451dfc010bafd990f1fa9d91003254af8b1c
Dylan Legendre <dylanlegendre09@gmail.com>
2016-11-23 15:11:30 -0600

    added unit testing framework NUnit and testing project

---
+++ EmpyrionManagementSuite/EMS.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EMS.Tests")]
+[assembly: AssemblyDescription("The Unit Testing Project for EMS")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MorpheusZero")]
+[assembly: AssemblyProduct("EMS.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("EMS")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("57906c8d-e88f-4bd7-88d6-d99791ce06f2")]
+[assembly: AssemblyVersion("1.0.*")]
